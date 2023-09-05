87f0e1fe4be744723832fbd7fb47ff12d7020475
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-04 19:18:28 -0500

    feat: :sparkles: server example

---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import http from "http";
+
+const HOSTAME = '127.0.0.1';
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+        data: "Hello World!",
+    }));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://${HOSTAME}:${PORT}/`);
+});
