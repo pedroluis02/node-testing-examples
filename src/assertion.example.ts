20de401ff831bf8613aeb82a632a6cb445015a0b
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-04 19:12:51 -0500

    feat: :sparkles: add assertion example

---
+++ src/assertion.example.ts
@@ -0,0 +1,22 @@
+import { SquareFigure } from "./figure";
+import { BankAccount } from "./bank";
+
+function example1() {
+    let figure = new SquareFigure(2, 4);
+    const area = figure.area();
+
+    console.log(figure);
+    console.log(`SquareFigure Area: ${area}`);
+}
+
+function example2() {
+    let account = new BankAccount('Pedro Luis', 100);
+    console.log(account);
+
+    account.deposit("20");
+
+    console.log(`Account after deposit: ${account}`);
+}
+
+example1();
+example2();
