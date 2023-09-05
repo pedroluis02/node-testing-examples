40120730c52a9b5a32ec776345c02c9eba488ff5
Pedro Luis <pedroluisnr22@gmail.com>
2023-09-04 23:41:53 -0500

    chore: :bento: add scripts configuration project

---
+++ setup-ts-jest.sh
@@ -0,0 +1,3 @@
+npm install --save-dev @babel/preset-env @babel/preset-typescript
+npm install --save-dev ts-jest @types/jest
+npx ts-jest config:init
---
+++ setup-ts-project.sh
@@ -0,0 +1,4 @@
+npm init
+npm istall --save-dev @types/node
+npm install typescript
+npx tsc --init
